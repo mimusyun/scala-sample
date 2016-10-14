5d302ce0a9b72de17f18151eae4162ecad78e265
shunichiromimura <shunichiromimura@192.168.100.41>
2016-10-14 20:28:16 +0900

    Optimized arguments

--- scala-sample/src/example/Main.scala
+++ scala-sample/src/example/Main.scala
@@ -3,23 +3,23 @@ package example
 object Main extends App {
    
   // Newton's method
+  
+  def abs(x: Double) = 
+      if (x > 0) x else -x
     
   def sqrt(x: Double) = { 
-    
-    def abs(x: Double) = 
-      if (x > 0) x else -x
       
-    def sqrtIter(guess: Double, x :Double): Double = 
-      if (isGoodEnough(guess, x)) guess
-      else sqrtIter(improve(guess, x), x)
+    def sqrtIter(guess: Double): Double = 
+      if (isGoodEnough(guess)) guess
+      else sqrtIter(improve(guess))
     
-    def isGoodEnough (guess: Double, x :Double) = 
+    def isGoodEnough (guess: Double) = 
       abs(guess * guess - x) < 1.0e-3
     
-    def improve(guess: Double, x :Double) =
+    def improve(guess: Double) =
       (guess + x / guess) / 2
       
-    sqrtIter(1.0, x)
+    sqrtIter(1.0)
   }
   
   println(sqrt(2))
