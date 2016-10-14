a46e78e4c7cdea22c91e8da5e8b8eb11eed31bba
shunichiromimura <shunichiromimura@192.168.100.41>
2016-10-14 22:16:31 +0900

    Added pascal's triangle generator

--- scala-sample/src/example/Main.scala
+++ scala-sample/src/example/Main.scala
@@ -21,12 +21,16 @@ object Main extends App {
     sqrtIter(1.0)
   }
   
+  println("#sqrt(2)")
   println(sqrt(2))
+  println()
   
   // Greatest common divisor
   def gcd(x: Int, y: Int): Int = if (y == 0) x else gcd(y, x % y)
   
+  println("#GCD(14, 21)")
   println(gcd(14, 21))
+  println()
 
   // Factorial - tail recursive version
   def factorial(n: Int): Int = {
@@ -36,6 +40,22 @@ object Main extends App {
     loop(1, n)
   }
   
+  println("#Factorial(5)")
   println(factorial(5))
+  println()
+  
+  def pascal(c: Int, r: Int): Int = {
+    if(c==0 || r==c) 1
+    else if(c<=r/2) pascal(c-1,r-1) + pascal(c,r-1)
+    else if(c>r/2) pascal(r-c-1,r) + pascal(r-c,r-1)
+    else 0
+  }
+  
+  println("#Pascal's Triangle")
+  for (row <- 0 to 10) {
+    for (col <- 0 to row)
+      print(pascal(col, row) + " ")
+      println()
+  }
   
 }
